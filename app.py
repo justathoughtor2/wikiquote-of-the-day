88ee3c306b39b177eda9937b1ccb0675fffe0ec2
Jesse Patrick Bohanan <thoughthaven@gmail.com>
2020-02-05 21:01:40 -0500

    Change to processing of text.

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import logging
-
 from io import BytesIO
 
 from flask import Flask
@@ -9,21 +7,28 @@ from requests_html import HTMLSession
 
 from PIL import Image, ImageDraw, ImageFont
 
+import textwrap
+
 import re
+
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def wikiquote():
     session = HTMLSession()
     r = session.get('https://en.wikiquote.org/wiki/Wikiquote:Quote_of_the_day?action=render')
-    wikitext = re.sub('(.{70,80} )', '\\1\n', r.html.find('div > center > table', first=True).text, flags=re.DOTALL)
-    height = 30 + 30 * wikitext.count('\n') if wikitext.count('\n') > 1 else 90
-    logging.info(wikitext)
+    wikitext = r.html.find('div > center > table', first=True).text
+    wikitext = wikitext.splitlines()
+    wikitext = '\n\n'.join('\n'.join(textwrap.wrap(text, width=110)) for text in wikitext)
+    height = 20 + 20 * wikitext.count('\n') if wikitext.count('\n') > 1 else 60
+    print(wikitext, file=sys.stdout)
     img = Image.new('RGB', (800,height), color='white')
     fnt = ImageFont.truetype('Montserrat-Regular.ttf', 14)
     d = ImageDraw.Draw(img)
-    d.multiline_text((10,10), wikitext, font=fnt, fill='black', spacing=6)
+    margin = offset = 10
+    d.text((margin, offset), wikitext, font=fnt, fill='black')
     output = BytesIO()
     img.save(output, format='PNG')
     output.seek(0)
@@ -33,5 +38,5 @@ def hello():
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception('An error occurred during a request.')
+    print('An error occurred during a request.', file=sys.stderr)
     return 'An internal error occurred.', 500
