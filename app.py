fe367468263432376b4099f0923a0fbc554cb743
Jesse P. Bohanan <thoughthaven@gmail.com>
2020-08-09 06:46:50 -0400

    Many, many changes (to implement Wikipic)

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import send_file
 
 from requests_html import HTMLSession
 
+import requests
+
 from PIL import Image, ImageDraw, ImageFont
 
 import textwrap
@@ -18,19 +20,48 @@ app = Flask(__name__)
 @app.route('/wikiquote-of-the-day')
 def wikiquote():
     session = HTMLSession()
-    r = session.get('https://en.wikiquote.org/wiki/Wikiquote:Quote_of_the_day?action=render')
-    wikitext = r.html.find('div > center > table', first=True).text
+    fnt = ImageFont.truetype('Montserrat-Regular.ttf', 56)
+    
+    r_pic = session.get('https://commons.wikimedia.org/wiki/Commons:Picture_of_the_day?action=render')
+
+    wikipic = 'https:' + r_pic.html.find('table > tbody > tr > td.toccolours > a', first=True).attrs['href']
+    print(wikipic, file=sys.stdout)
+
+    r_pic_true = session.get(wikipic).html.find('div.fullImageLink > a', first=True).attrs['href']
+    print(r_pic_true, file=sys.stdout)
+
+    response = requests.get(r_pic_true)
+    img_pic = Image.open(BytesIO(response.content)).convert('RGBA')
+
+    r_quote = session.get('https://en.wikiquote.org/wiki/Wikiquote:Quote_of_the_day?action=render')
+
+    wikitext = r_quote.html.find('div > center > table', first=True).text
     wikitext = wikitext.splitlines()
-    wikitext = '\n\n'.join('\n'.join(textwrap.wrap(text, width=110)) for text in wikitext)
-    height = 20 + 20 * wikitext.count('\n') if wikitext.count('\n') > 1 else 60
+    wikitext = '\n\n'.join('\n'.join(textwrap.wrap(text, width=120)) for text in wikitext)
+    height = 80 + 80 * wikitext.count('\n') if wikitext.count('\n') > 1 else 240
     print(wikitext, file=sys.stdout)
-    img = Image.new('RGB', (800,height), color='white')
-    fnt = ImageFont.truetype('Montserrat-Regular.ttf', 14)
-    d = ImageDraw.Draw(img)
-    margin = offset = 10
+    img_text = Image.new('RGBA', (img_pic.size), color=(255, 255, 255, 62))
+    d = ImageDraw.Draw(img_text, mode='RGBA')
+    margin = img_pic.size[0] / 16
+    offset = img_pic.size[1] / 8
     d.text((margin, offset), wikitext, font=fnt, fill='black')
+    
+    wikipic_caption = r_pic.html.find('table > tbody > tr > td > div.description', first=True).text
+    wikipic_caption = wikipic_caption.splitlines()
+    wikipic_caption = '\n\n'.join('\n'.join(textwrap.wrap(text, width=120)) for text in wikipic_caption)
+    height = 80 + 80 * wikipic_caption.count('\n') if wikipic_caption.count('\n') > 1 else 240
+    print(wikipic_caption, file=sys.stdout)
+    img_caption = Image.new('RGBA', (img_pic.size), color=(255, 255, 255, 62))
+    d2 = ImageDraw.Draw(img_caption, mode='RGBA')
+    margin = img_pic.size[0] / 16
+    offset = (img_pic.size[1] / 8) * 7
+    d2.text((margin, offset), wikipic_caption, font=fnt, fill='black')
+
+    out = Image.alpha_composite(img_pic, img_text)
+    out2 = Image.alpha_composite(out, img_caption)
+
     output = BytesIO()
-    img.save(output, format='PNG')
+    out2.save(output, format='PNG')
     output.seek(0)
     resp = send_file(output, mimetype='image/png')
     resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
